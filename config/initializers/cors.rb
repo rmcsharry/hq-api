4aed347dd3c12a3697886398e4264f3ec6bc2b73
Arne Zeising <arne.zeising@gmail.com>
2018-01-31 15:58:44 +0100

    Allow requests from dev app URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'localhost:3001', 'app-dev.hqtrust-dev.sherpasdv.de'
 
     resource '*',
              headers: :any,
