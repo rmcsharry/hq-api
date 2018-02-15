a839bfa18f9ac4c57365ff5ced4d6eefa6a83854
Arne Zeising <arne.zeising@gmail.com>
2018-02-15 12:47:07 +0100

    Try to use env var directly

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,10 +1,4 @@
-if Rails.application.secrets.sentry_dsn.present?
-  Raven.configure do |config|
-    p Rails.application.secrets.sentry_dsn
-    p Rails.application.secrets.sentry_dsn.class
-    uri = URI.parse(Rails.application.secrets.sentry_dsn)
-    p uri
-    config.dsn = Rails.application.secrets.sentry_dsn
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  end
+Raven.configure do |config|
+  config.dsn = ENV["SENTRY_DSN"]
+  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
