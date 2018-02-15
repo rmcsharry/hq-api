338e239cebca514a5e7731f89650c8869ded5bc9
Arne Zeising <arne.zeising@gmail.com>
2018-02-15 13:29:54 +0100

    Correctly filter parameters

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,5 @@ module HqtrustCoreApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.filter_parameters << %i[email password]
   end
 end
