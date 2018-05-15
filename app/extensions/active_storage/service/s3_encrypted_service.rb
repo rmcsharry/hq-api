63dce926ea4fc3212a9b18e859fcb29fc52d9c14
Arne Zeising <arne.zeising@gmail.com>
2018-05-15 19:18:40 +0200

    Change Service from module to class

--- app/extensions/active_storage/service/s3_encrypted_service.rb
+++ app/extensions/active_storage/service/s3_encrypted_service.rb
@@ -4,7 +4,7 @@ require 'aws-sdk-s3'
 require 'active_support/core_ext/numeric/bytes'
 
 module ActiveStorage
-  module Service
+  class Service
     # Wraps the Amazon Simple Storage Service (S3) as an Active Storage service.
     # See ActiveStorage::Service for the generic API documentation that applies to all services.
     class S3EncryptedService < Service
