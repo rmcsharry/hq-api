24f3e9d4d4b375fc5cdfe7c9be6d19af97539ed7
Arne Zeising <arne.zeising@gmail.com>
2018-12-11 09:11:48 +0100

    Adapt to new FactoryBot format

--- spec/factories/investor_cashflow.rb
+++ spec/factories/investor_cashflow.rb
@@ -8,24 +8,24 @@ FactoryBot.define do
 
     investor { build(:investor, fund: fund) }
     fund_cashflow { build(:fund_cashflow, fund: fund) }
-    aasm_state 'open'
+    aasm_state { 'open' }
 
     trait :capital_call do
-      capital_call_compensatory_interest_amount 100_000
-      capital_call_gross_amount 100_000
-      capital_call_management_fees_amount 100_000
+      capital_call_compensatory_interest_amount { 100_000 }
+      capital_call_gross_amount { 100_000 }
+      capital_call_management_fees_amount { 100_000 }
     end
 
     trait :distribution do
-      distribution_compensatory_interest_amount 100_000
-      distribution_dividends_amount 100_000
-      distribution_interest_amount 100_000
-      distribution_misc_profits_amount 100_000
-      distribution_participation_profits_amount 100_000
-      distribution_recallable_amount 100_000
-      distribution_reduction_amount 100_000
-      distribution_structure_costs_amount 100_000
-      distribution_withholding_tax_amount 100_000
+      distribution_compensatory_interest_amount { 100_000 }
+      distribution_dividends_amount { 100_000 }
+      distribution_interest_amount { 100_000 }
+      distribution_misc_profits_amount { 100_000 }
+      distribution_participation_profits_amount { 100_000 }
+      distribution_recallable_amount { 100_000 }
+      distribution_reduction_amount { 100_000 }
+      distribution_structure_costs_amount { 100_000 }
+      distribution_withholding_tax_amount { 100_000 }
     end
   end
 end
