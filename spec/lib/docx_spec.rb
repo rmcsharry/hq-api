0313479bb8aafd5bde9fdf2f37c4879e97ce6bbd
Tom König <tomknig@users.noreply.github.com>
2019-01-11 18:19:47 +0100

    AI-35 & 36: Preserve Line breaks & support for quill deltas (#229)
    * Only delete actual match_strings to retain linebreaks
    
    * Satisfy rubocop
    
    * Retain trailing spaces by changing replace order
    
    * Add support for rendering quill-deltas into docx
    
    * Preserve whitespaces in generated xml

--- spec/lib/docx_spec.rb
+++ spec/lib/docx_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Docx::Document, type: :util do
   let(:file_path) { Rails.root.join('spec', 'fixtures', 'docx', document_name) }
 
   describe 'document parsing' do
-    context 'with actual template' do
+    context 'with `Ausschuettung` template' do
       let(:document_name) { '20181219-Ausschuettung_Vorlage.docx' }
 
       it 'properly parses the template' do
@@ -32,6 +32,64 @@ RSpec.describe Docx::Document, type: :util do
         expect(content).to include('421337')
         expect(content).to include('24.12.2018')
         expect(content).not_to match(/\{[a-z_\.]+\}/)
+      end
+    end
+
+    context 'with `Quartalsbericht` template' do
+      let(:document_name) { '20181219-Quartalsbericht_Vorlage.docx' }
+
+      it 'properly parses the template' do
+        document = Docx::Document.new(file_path)
+
+        description = Quill::Delta.new <<-QUILL.squish
+          {
+            "ops": [
+              { "insert": "Quill", "attributes": { "bold": true } },
+              { "insert": " text " },
+              { "insert": "here.", "attributes": { "italic": true } },
+              { "insert": " text\\nfirst list entry" },
+              { "insert": "\\n", "attributes": { "list": "bullet" } },
+              { "insert": "second list entry" },
+              { "insert": "\\n", "attributes": { "list": "bullet" } }
+            ]
+          }
+        QUILL
+
+        document.commit(
+          current_date: '01.01.2019',
+          fund: {
+            currency: 'EUR',
+            name: 'Fancy Foo Funds'
+          },
+          fund_report: {
+            description: description.to_s
+          },
+          investor: {
+            amount_total: 1337,
+            contact_address: {
+              city: 'Berlin',
+              postal_code: 10_997,
+              street_and_number: 'Visionsstr. 42'
+            },
+            primary_owner: {
+              formal_salutation: 'Sehr geehrter Herr',
+              full_name: 'Donald Knuth',
+              gender: 'Herr'
+            }
+          }
+        )
+
+        tempfile = Tempfile.new
+        tempfile.binmode
+        tempfile.write document.render
+        rendered_document = Docx::Document.new(tempfile.path)
+        tempfile.close
+
+        content = rendered_document.to_s
+        expect(content).to include('Zeichnungsbetrag: 1337')
+        expect(content).to include('Quill text here.')
+        expect(content).to include('first list entry')
+        expect(content).to include('second list entry')
       end
     end
   end
---
+++ spec/lib/quill_spec.rb
@@ -0,0 +1,170 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Quill::Delta, type: :util do
+  def normalize_xml(xml_string)
+    xml_string.gsub(/>\s</, '><')
+  end
+
+  describe 'delta string parsing' do
+    it 'handles malformed delta strings' do
+      delta = Quill::Delta.new 'this is not a quill delta'
+
+      expect(delta.to_s).to eq 'this is not a quill delta'
+    end
+
+    it 'handles bold-attribute and removes trailing newline' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill\\n" }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+
+    it 'handles bold-attribute' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill\\n", "attributes": { "bold": true } }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:rPr>
+              <w:b/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+
+    it 'handles italic-attribute' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill\\n", "attributes": { "italic": true } }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:rPr>
+              <w:i/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+
+    it 'handles disabled attributes' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill\\n", "attributes": { "bold": true, "italic": false } }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:rPr>
+              <w:b/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+
+    it 'handles multiple attributes' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill\\n", "attributes": { "bold": true, "italic": true } }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:rPr>
+              <w:b/>
+              <w:i/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+
+    it 'handles bullet lists' do
+      delta = Quill::Delta.new <<-QUILL.squish
+        {
+          "ops": [
+            { "insert": "Quill", "attributes": { "italic": true } },
+            { "insert": " text\\nlist" },
+            { "insert": " entry", "attributes": { "bold": true } },
+            { "insert": "\\n", "attributes": { "list": "bullet" } },
+            { "insert": "the end\\n" }
+          ]
+        }
+      QUILL
+
+      expect(normalize_xml(delta.to_s)).to eq normalize_xml <<-EXPECTATION.squish
+        <w:p>
+          <w:r>
+            <w:rPr>
+              <w:i/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\">Quill</w:t>
+          </w:r>
+          <w:r>
+            <w:t xml:space=\"preserve\"> text</w:t>
+          </w:r>
+        </w:p>
+        <w:p>
+          <w:pPr>
+            <w:numPr>
+              <w:ilvl w:val="0"/>
+              <w:numId w:val="1"/>
+            </w:numPr>
+          </w:pPr>
+          <w:r>
+            <w:t xml:space=\"preserve\">list</w:t>
+          </w:r>
+          <w:r>
+            <w:rPr>
+              <w:b/>
+            </w:rPr>
+            <w:t xml:space=\"preserve\"> entry</w:t>
+          </w:r>
+        </w:p>
+        <w:p>
+          <w:r>
+            <w:t xml:space=\"preserve\">the end</w:t>
+          </w:r>
+        </w:p>
+      EXPECTATION
+    end
+  end
+end
