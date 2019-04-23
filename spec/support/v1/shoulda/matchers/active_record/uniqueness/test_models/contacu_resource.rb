3fcd62982fdc6978b6b29171ae96de5a46c0279e
Arne Zeising <arne.zeising@gmail.com>
2019-04-23 18:23:41 +0200

    CRM-539: Sort mandate owners by name (#321)
    * CRM-539: Sort mandate owners by name
    
    * Move psych to general dependency block
    
    * Fix specs, revert psych
    
    * Try to stub model class

--- spec/support/v1/shoulda/matchers/active_record/uniqueness/test_models/contacu_resource.rb
+++ spec/support/v1/shoulda/matchers/active_record/uniqueness/test_models/contacu_resource.rb
@@ -8,6 +8,9 @@ module V1
           module TestModels
             class ContacuResource
               # Stub to fix this issue: https://github.com/HQTrust/hqtrust-core-api/pull/318
+              def self._model_class
+                Contact
+              end
             end
           end
         end
