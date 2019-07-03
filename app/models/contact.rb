f18d5c20093d48f22163b17ccc22298efe25e74e
Richard McSharry <rmcsharry@gmail.com>
2019-07-04 01:29:29 +0200

    Removes scoring from parent contact class

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -47,7 +47,6 @@
 # rubocop:disable Metrics/ClassLength
 class Contact < ApplicationRecord
   include ExportableAttributes
-  include IntegrityScoring
   extend Enumerize
   strip_attributes only: %i[
     commercial_register_number commercial_register_office first_name last_name maiden_name nationality
