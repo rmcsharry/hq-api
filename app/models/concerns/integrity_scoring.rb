41ebdde7668b0a1b8ad42dfc7b7045a2a230ac80
Richard McSharry <rmcsharry@gmail.com>
2019-07-04 15:34:50 +0200

    DRY the code a little

--- app/models/concerns/integrity_scoring.rb
+++ app/models/concerns/integrity_scoring.rb
@@ -38,71 +38,47 @@ module IntegrityScoring
   end
 
   def accumulate_score_by_weight
-    if @weight[:model_key] == model_name.param_key
-      from_me # attribute is on the model itself
-    else
-      from_relative # attribute is on a related model
-    end
+    return from_me if @weight[:model_key] == model_name.param_key # attribute is on the model itself
+
+    from_relative # attribute is on a related model
   end
 
   def from_me
+    weight_name = @weight[:name]
     field_name = @weight[:name].camelize(:lower)
-    if self.class.method_defined? @weight[:name]
-      absolute_weight(field_name, :from_method?)
+    if self.class.method_defined?(weight_name)
+      absolute_weight(field_name, public_send(weight_name).present?) # apply weight if method returns a value
     else
-      absolute_weight(field_name, :from_attribute?)
+      absolute_weight(field_name, self[weight_name].present?) # apply weight if attribute returns a value
     end
-  end
-
-  def from_method?
-    public_send(@weight[:name]).present?
-  end
-
-  def from_attribute?
-    self[@weight[:name]].present?
   end
 
   def from_relative
-    if @weight[:name].include?('==')
-      # search_for_field # search the relative for a particular field
-      search_for_field
-    else
-      direct_from_relative # directly check the relative
-    end
+    return search_for_field if @weight[:name].include?('==') # search the relative for a particular field
+
+    direct_from_relative # directly check the relative
   end
 
   def search_for_field
-    absolute_weight(@weight[:name].split('==')[0], :related_field_has_value?)
-  end
-
-  def related_field_has_value?
     field, value = @weight[:name].split('==')
-    public_send(@weight[:model_key]).where("#{field}": value).present?
+    absolute_weight(field, public_send(@weight[:model_key]).where("#{field}": value).present?)
   end
 
   def direct_from_relative
-    if @weight[:name] == ''
-      absolute_weight(@weight[:model_key], :at_least_one_record?)
+    weight_name = @weight[:name]
+    key = @weight[:model_key]
+    if weight_name == ''
+      absolute_weight(key, public_send(key).present?) # at least one record for relative
     else
-      absolute_weight(@weight[:name].camelize(:lower), :specific_field?)
+      absolute_weight(weight_name.camelize(:lower), public_send(key)[weight_name].present?) # specific field for relative
     end
   end
 
-  def at_least_one_record?
-    public_send(@weight[:model_key]).present?
-  end
+  def absolute_weight(field, is_present)
+    # if the value is present, calculate the absolute weight
+    return @weight[:relative_weight] / self.class.relative_weights_total if is_present
 
-  def specific_field?
-    public_send(@weight[:model_key])[@weight[:name]].present?
-  end
-
-  def absolute_weight(field, presence_checker)
-    if send(presence_checker)
-      # if the value is present, calculate the absolute weight
-      @weight[:relative_weight] / self.class.relative_weights_total
-    else
-      @missing_fields << field
-      0
-    end
+    @missing_fields << field
+    0
   end
 end
