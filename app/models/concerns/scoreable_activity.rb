4e83925e0cf3330d07819632cb4b5077d7a76469
Richard McSharry <rmcsharry@gmail.com>
2019-07-11 23:28:19 +0200

    Removes commented association code

--- app/models/concerns/scoreable_activity.rb
+++ app/models/concerns/scoreable_activity.rb
@@ -4,24 +4,6 @@
 module ScoreableActivity
   extend ActiveSupport::Concern
 
-  # included do
-  #   has_and_belongs_to_many :contacts, -> { distinct }
-  #   has_and_belongs_to_many :mandates, -> { distinct }
-
-  #   before_add_for_mandates << lambda do |_hook, _activity, object|
-  #     rule = object.class.get_rule(model_key: 'activities', name: '')
-  #     object.class.process_rule(instance: object, rule: rule, direction: 1) if object.activities.zero?
-  #   end
-  #   before_add_for_contacts << lambda do |_hook, _activity, object|
-  #     rule = object.class.get_rule(model_key: 'activities', name: '')
-  #     object.class.process_rule(instance: object, rule: rule, direction: 1) if object.activities.zero?
-  #   end
-  #   # before_remove_for_contacts << lambda do |_hook, _activity, object|
-  #   #   rule = object.class.get_rule(model_key: 'activities', name: '')
-  #   #   object.class.process_rule(instance: object, rule: rule, direction: -1) if object.activities.count == 1
-  #   # end
-  # end
-
   attr_accessor :contacts_to_recalculate, :mandates_to_recalculate
 
   included do
