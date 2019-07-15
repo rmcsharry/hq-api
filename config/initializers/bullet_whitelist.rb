969570190b9763d608f4eac20840dba43812ed06
Richard McSharry <rmcsharry@gmail.com>
2019-07-15 14:50:16 +0200

    Adds contact details n+1 to bullet whitelist

--- config/initializers/bullet_whitelist.rb
+++ config/initializers/bullet_whitelist.rb
@@ -3,4 +3,6 @@
 if defined? Bullet
   Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Contact::Person', association: :tax_detail
   Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Contact::Organization', association: :tax_detail
+  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'ContactDetail::Phone', association: :contact
+  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'ContactDetail::Email', association: :contact
 end
