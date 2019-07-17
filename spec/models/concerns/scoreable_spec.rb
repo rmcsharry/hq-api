bca56891be2f7be723032da93bfa8cf5c19941f4
Richard McSharry <rmcsharry@gmail.com>
2019-07-17 18:44:19 +0200

    Adds scoreable activities tests for organization

--- spec/models/concerns/scoreable_spec.rb
+++ spec/models/concerns/scoreable_spec.rb
@@ -220,6 +220,65 @@ RSpec.describe Scoreable, bullet: false do
         end
       end
 
+      context 'when related model changes' do
+        # NOTE
+        # We create instead of build, to ensure the after_save callback fires, giving the correct starting score
+        # instead of the random score from the contact factory
+        let!(:subject) { create(:contact_organization) }
+        let!(:activity_1) { create(:activity_note) }
+        let!(:activity_2) { create(:activity_note) }
+
+        it 'scores correctly when initial activity is added' do
+          activity_1.contacts << subject
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(20)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.2547)
+        end
+
+        it 'scores correctly when final activity is removed' do
+          activity_1.contacts << subject
+          activity_1.contacts.destroy(subject)
+
+          expect(subject.data_integrity_missing_fields).to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(21)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
+        end
+
+        it 'scores correctly when final activity itself is destroyed' do
+          activity_1.contacts << subject
+          activity_1.destroy!
+
+          expect(subject.data_integrity_missing_fields).to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(21)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
+        end
+
+        it 'does not rescore when adding activites after the first one' do
+          activity_1.contacts << subject
+          activity_1.save!
+          stub_const('Contact', double)
+          activity_2.contacts << subject
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(20)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.2547)
+        end
+
+        it 'does not rescore when removing activites except one' do
+          activity_1.contacts << subject
+          activity_1.save!
+          activity_2.contacts << subject
+          activity_2.save!
+          stub_const('Contact', double)
+          activity_1.destroy!
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(20)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.2547)
+        end
+      end
+
       context 'when organization is a mandate owner' do
         it 'rescores the mandate when the owner score changes' do
         end
