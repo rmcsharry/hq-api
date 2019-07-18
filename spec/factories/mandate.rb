afed69ae058c77a2ab3a6cc73f183c0a08ccfe9f
Richard McSharry <rmcsharry@gmail.com>
2019-07-18 10:56:32 +0200

    Adds mandate scoreable tests

--- spec/factories/mandate.rb
+++ spec/factories/mandate.rb
@@ -30,5 +30,30 @@ FactoryBot.define do
         ]
       end
     end
+
+    trait :with_bank_account do
+      bank_accounts do
+        [
+          create(:bank_account, owner: @instance, iban: 'DE12500105170648489890' )
+        ]
+      end
+    end
+
+    trait :with_scoreable_data do
+      datev_creditor_id { Faker::Number.number(10) }
+      datev_debitor_id { Faker::Number.number(10) }
+      mandate_number { "#{Faker::Number.number(3)}-#{Faker::Number.number(3)}-#{Faker::Number.number(3)}" }
+      psplus_id { Faker::Number.number(9) }
+      valid_from { Faker::Date.between(15.years.ago, Time.zone.today) }
+      mandate_members do
+        [
+          create(:mandate_member, mandate: @instance, member_type: :primary_consultant),
+          create(:mandate_member, mandate: @instance, member_type: :secondary_consultant),
+          create(:mandate_member, mandate: @instance, member_type: :assistant),
+          create(:mandate_member, mandate: @instance, member_type: :bookkeeper),
+          # create(:mandate_member, mandate: @instance, contact: owner)
+        ]
+      end
+    end
   end
 end
--- spec/models/concerns/scoreable_spec.rb
+++ spec/models/concerns/scoreable_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Scoreable, bullet: false do
 
         it 'scores minimum' do
           # NOTE
-          # first_name, last_name and gender are the minimum required fields for a contact
-          # hence the lowest score is the weights for those 3 fields (not zero)
+          # we check the minimum required fields for a contact
+          # hence the lowest score is the weights for those fields (not zero)
           subject.calculate_score
 
           expect(subject.data_integrity_missing_fields).not_to include('first_name', 'last_name', 'date_of_birth')
@@ -73,7 +73,6 @@ RSpec.describe Scoreable, bullet: false do
           subject.compliance_detail = build(:compliance_detail, contact: subject)
           subject.tax_detail = build(:tax_detail, :with_scoreable_person_data)
           subject.calculate_score
-          puts subject.data_integrity_missing_fields
 
           expect(subject.data_integrity_missing_fields.length).to eq(0)
           expect(subject.data_integrity_score).to be_within(0.0001).of(1.0)
@@ -151,8 +150,8 @@ RSpec.describe Scoreable, bullet: false do
 
         it 'scores minimum' do
           # NOTE
-          # name and type are the minimum required fields for an organization
-          # hence the lowest score is the weights for those 2 fields (not zero)
+          # we check the minimum required fields for an organization
+          # hence the lowest score is the weights for those fields (not zero)
           subject.calculate_score
 
           expect(subject.data_integrity_missing_fields).not_to include('organization_name', 'organization_type')
@@ -281,6 +280,151 @@ RSpec.describe Scoreable, bullet: false do
 
       context 'when organization is a mandate owner' do
         it 'rescores the mandate when the owner score changes' do
+        end
+      end
+    end
+
+    describe 'for mandate' do
+      context 'when some rules apply' do
+        let!(:subject) { build(:mandate) }
+
+        it 'scores minimum' do
+          # NOTE
+          # we check the minimum required fields for a mandate
+          # hence the lowest score is the weights for those fields (not zero)
+          subject.calculate_score
+          puts subject.data_integrity_missing_fields
+
+          expect(subject.data_integrity_missing_fields).not_to include(
+            'category', 'state', 'primary_consultant', 'secondary_consultant'
+          )
+          expect(subject.data_integrity_missing_fields.length).to eq(11)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2597)
+        end
+
+        it 'is correct when rule: a specific property from the main model is filled' do
+          subject.psplus_id = 'A1234567890'
+          subject.calculate_score
+
+          expect(subject.data_integrity_missing_fields).not_to include('psplus_id')
+          expect(subject.data_integrity_missing_fields.length).to eq(10)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.1364)
+        end
+
+        it 'is correct when rule: a related model has at least one record' do
+          subject.activities << build(:activity_note)
+          subject.calculate_score
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(10)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2403)
+        end
+
+        it 'is correct when rule: a related model is searched for a specific value' do
+          subject.documents << build(:document, category: 'contract_hq')
+          subject.save # needed to ensure the document type is found by the algorithm
+          subject.calculate_score
+
+          expect(subject.data_integrity_missing_fields).not_to include('contract_hq')
+          expect(subject.data_integrity_missing_fields.length).to eq(10)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2273)
+        end
+      end
+
+      context 'when all rules apply' do
+        let!(:subject) { create(:mandate, :with_bank_account, :with_scoreable_data) }
+        let!(:contact) { create(:contact_person, :with_contact_details, :with_scoreable_data) }
+
+        it 'scores maximum partial score and 50% score (with no owner)' do
+          subject.activities << build(:activity_note)
+          subject.documents << build(:document, category: 'contract_hq')
+          subject.calculate_score
+
+          expect(subject.data_integrity_missing_fields.length).to eq(0)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.5)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(1.0)
+        end
+
+        it 'scores maximum partial score and maximum score (with 1 maximum scored owner)' do
+          contact.activities << build(:activity_note)
+          contact.documents << build(:document, category: 'kyc')
+          contact.compliance_detail = build(:compliance_detail, contact: contact)
+          contact.tax_detail = build(:tax_detail, :with_scoreable_person_data)
+          contact.reload
+          contact.calculate_score
+          contact.save!
+          expect(contact.data_integrity_missing_fields.length).to eq(0)
+          expect(contact.data_integrity_score).to be_within(0.0001).of(1.0)
+
+          subject.activities << create(:activity_note)
+          subject.documents << create(:document, category: 'contract_hq')
+          subject.mandate_members << create(:mandate_member, mandate: subject, contact: contact)
+          subject.reload # so scoreable can see the subject.owners
+          subject.calculate_score
+          subject.save!
+
+          expect(subject.data_integrity_missing_fields.length).to eq(0)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(1.0)
+          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(1.0)
+        end
+      end
+
+      context 'when related model changes' do
+        # NOTE
+        # We create instead of build, to ensure the after_save callback fires, giving the correct starting score
+        # instead of the random score from the contact factory
+        let!(:subject) { create(:contact_person) }
+        let!(:activity_1) { create(:activity_note) }
+        let!(:activity_2) { create(:activity_note) }
+
+        it 'scores correctly when initial activity is added' do
+          activity_1.contacts << subject
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(23)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
+        end
+
+        it 'scores correctly when final activity is removed' do
+          activity_1.contacts << subject
+          activity_1.contacts.destroy(subject)
+
+          expect(subject.data_integrity_missing_fields).to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(24)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
+        end
+
+        it 'scores correctly when final activity itself is destroyed' do
+          activity_1.contacts << subject
+          activity_1.destroy!
+
+          expect(subject.data_integrity_missing_fields).to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(24)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
+        end
+
+        it 'does not rescore when adding activites after the first one' do
+          activity_1.contacts << subject
+          activity_1.save!
+          stub_const('Contact', double)
+          activity_2.contacts << subject
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(23)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
+        end
+
+        it 'does not rescore when removing activites except one' do
+          activity_1.contacts << subject
+          activity_1.save!
+          activity_2.contacts << subject
+          activity_2.save!
+          stub_const('Contact', double)
+          activity_1.destroy!
+
+          expect(subject.data_integrity_missing_fields).not_to include('activities')
+          expect(subject.data_integrity_missing_fields.length).to eq(23)
+          expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
         end
       end
     end
