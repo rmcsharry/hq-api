88973a57b0c1dbad17446a1439414564d9d9dad2
Richard McSharry <rmcsharry@gmail.com>
2019-07-19 09:32:35 +0200

    Finishes polymorhpic refactor of Rule factory

--- app/models/concerns/rule.rb
+++ app/models/concerns/rule.rb
@@ -3,23 +3,25 @@
 # Rule is an abstract factory class (not meant to be instantiated directly)
 # Used to build rules based on its descendant classes and apply those rules to objects (contacts, mandates etc)
 #
-# HOW TO USE
+# INPUT
 # Call Rule.build and provide:
 #  1 an object
 #  2 a single rule
 #
-# RETURNS
-#  an instance of a descendant class matching the given rule type
-#
-# Calling 'result' on that instance returns a hash:
-# {score: x, name: field to show to user}
-# If the rule does not apply, the result will be {score: 0.0, name: field to show to user}
+# OUTPUT
+#  an instance of the descendant class that matches the given rule type
+#  call result on the instance -> returns hash: {score: x, name: field scored}
+# 
+# If the rule does not apply, the result will be {score: 0.0, name: field scored}
 class Rule
+  attr_accessor :field_name, :relative_weight, :weights_total
+
   def initialize(object:, rule:)
     @object = object
     @model = rule[:model_key]
     @property = rule[:name]
     @relative_weight = rule[:relative_weight]
+    @weights_total = object.class.relative_weights_total
   end
 
   def self.build(object:, rule:)
@@ -34,47 +36,44 @@ class Rule
     @descendants ||= []
   end
 
-  private
-
-  def absolute_weight(field_name, presence_checker)
-    if send(presence_checker)
-      { score: (@relative_weight / @object.class.relative_weights_total), name: field_name }
-    else
-      { score: 0.0, name: field_name }
-    end
+  def valid?
+    !@object.public_send(@model).nil?
   end
 
   # rubocop:disable Style/Documentation
   class MainProperty < Rule
     def result
-      absolute_weight(@property.camelize(:lower), :main_property_present?)
+      @field_name = @property.camelize(:lower)
+      Score.build(self).result
     end
 
-    def main_property_present?
+    def passed?
       @object.public_send(@property).present?
+    end
+
+    def valid?
+      true # rules on main are always valid (else dev error)
     end
   end
 
   class RelativeProperty < Rule
     def result
-      absolute_weight(@property.camelize(:lower), :specific_property_present?)
+      @field_name = @property.camelize(:lower)
+      Score.build(self).result
     end
 
-    def specific_property_present?
-      return false if @object.public_send(@model).nil?
-
+    def passed?
       @object.public_send(@model)[@property].present?
     end
   end
 
   class RelativeFieldValue < Rule
     def result
-      absolute_weight(@property.split('==')[1], :field_value_present?)
+      _, @field_name = @property.split('==')
+      Score.build(self).result
     end
 
-    def field_value_present?
-      return false if @object.public_send(@model).nil?
-
+    def passed?
       field, value = @property.split('==')
       @object.public_send(@model).where("#{field}": value).present?
     end
@@ -82,13 +81,60 @@ class Rule
 
   class RelativeAtLeastOne < Rule
     def result
-      absolute_weight(@model, :at_least_one_present?)
+      @field_name = @model
+      Score.build(self).result
     end
 
-    def at_least_one_present?
-      return false if @object.public_send(@model).nil?
-
+    def passed?
       @object.public_send(@model).present?
+    end
+  end
+
+  class Score
+    def initialize(rule)
+      @rule = rule
+    end
+
+    def self.build(rule)
+      descendants.detect { |klass| klass.match? rule }.new(rule)
+    end
+
+    def self.inherited(klass)
+      descendants.push klass
+    end
+
+    def self.descendants
+      @descendants ||= []
+    end
+
+    class InvalidZero < Score
+      def self.match?(rule)
+        !rule.valid?
+      end
+
+      def result
+        { name: @rule.field_name, score: 0.0 }
+      end
+    end
+
+    class ValidZero < Score
+      def self.match?(rule)
+        rule.valid? && !rule.passed?
+      end
+
+      def result
+        { name: @rule.field_name, score: 0.0 }
+      end
+    end
+
+    class NotZero < Score
+      def self.match?(rule)
+        rule.valid? && rule.passed?
+      end
+
+      def result
+        { name: @rule.field_name, score: (@rule.relative_weight / @rule.weights_total) }
+      end
     end
   end
   # rubocop:enable Style/Documentation
