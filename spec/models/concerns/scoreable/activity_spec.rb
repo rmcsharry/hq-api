d09b48ced0945ab2d2a0c6a1c4094e0fae1b1024
Richard McSharry <rmcsharry@gmail.com>
2019-08-20 17:33:44 +0200

    Refactors activity scoreable spec using let/before

--- spec/models/concerns/scoreable/activity_spec.rb
+++ spec/models/concerns/scoreable/activity_spec.rb
@@ -10,63 +10,59 @@ RSpec.describe Scoreable::Activity, bullet: false do
         # We create instead of build, to ensure the after_save callback fires, giving the correct starting score
         # instead of the random score from the contact factory
         subject { create(:contact_person) }
-        let!(:activity_1) { create(:activity_note) }
+        let!(:activity_1) { create(:activity_note, contacts: contact_list) }
         let!(:activity_2) { create(:activity_note) }
+        let!(:contact_list) { [] }
 
-        before do
+        before :each do
           activity_1.contacts << subject
         end
 
-        it 'scores correctly when initial activity is added' do
-          expect(subject.data_integrity_missing_fields).not_to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(23)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
-        end
-
-        it 'scores correctly when final activity is removed' do
-          activity_1.contacts.destroy(subject)
-          activity_1.save!
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(24)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
-        end
-
-        it 'scores correctly when final activity itself is destroyed' do
-          activity_1.destroy!
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(24)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
-        end
-
-        context 'when adding activites after the first one' do
-          before do
-            activity_1.save!
-            stub_const('Contact', double)
-            activity_2.contacts << subject
-          end
-
-          it 'does not rescore' do
+        describe 'when initial activity is added' do
+          it 'scores correctly' do
             expect(subject.data_integrity_missing_fields).not_to include('activities')
             expect(subject.data_integrity_missing_fields.length).to eq(23)
             expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
           end
         end
 
-        context 'when removing activites (rule: at least one)' do
+        describe 'when final activity is removed' do
           before do
-            activity_1.save!
-            activity_2.contacts << subject
-            activity_2.save!
-            stub_const('Contact', double)
+            activity_1.contacts.destroy(subject)
+          end
+
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(24)
+            expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
+          end
+        end
+
+        describe 'when final activity itself is destroyed' do
+          before do
             activity_1.destroy!
           end
 
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(24)
+            expect(subject.data_integrity_score).to be_within(0.0001).of(0.1626)
+          end
+        end
+
+        describe 'when adding activites after the first one' do
           it 'does not rescore' do
-            expect(subject.data_integrity_missing_fields).not_to include('activities')
-            expect(subject.data_integrity_missing_fields.length).to eq(23)
-            expect(subject.data_integrity_score).to be_within(0.0001).of(0.3469)
+            expect(subject).not_to receive(:calculate_score)
+            activity_2.contacts << subject
+          end
+        end
+
+        describe 'when removing activites beyond the first' do
+          let!(:activity_2) { create(:activity_note, contacts: [subject]) }
+
+          it 'does not rescore' do
+            expect(subject).not_to receive(:calculate_score)
+            activity_1.destroy!
           end
         end
       end
@@ -78,48 +74,55 @@ RSpec.describe Scoreable::Activity, bullet: false do
         # We create instead of build, to ensure the after_save callback fires, giving the correct starting score
         # instead of the random score from the contact factory
         subject { create(:contact_organization) }
-        let!(:activity_1) { create(:activity_note) }
+        let!(:activity_1) { create(:activity_note, contacts: contact_list) }
         let!(:activity_2) { create(:activity_note) }
+        let!(:contact_list) { [] }
 
-        before do
+        before :each do
           activity_1.contacts << subject
         end
 
-        it 'scores correctly when initial activity is added' do
-          expect(subject.data_integrity_missing_fields).not_to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(20)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.2547)
-        end
-
-        it 'scores correctly when final activity is removed' do
-          activity_1.contacts.destroy(subject)
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(21)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
-        end
-
-        it 'scores correctly when final activity itself is destroyed' do
-          activity_1.destroy!
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(21)
-          expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
-        end
-
-        it 'does not rescore when adding activites after the first one' do
-          activity_1.save!
-
-          expect(subject).not_to receive(:calculate_score)
-          activity_2.contacts << subject
-        end
-
-        context 'when removing activites (rule: at least one)' do
-          before do
-            activity_1.save!
-            activity_2.contacts << subject
-            activity_2.save!
+        describe 'when initial activity is added' do
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).not_to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(20)
+            expect(subject.data_integrity_score).to be_within(0.0001).of(0.2547)
           end
+        end
+
+        describe 'when final activity is removed' do
+          before do
+            activity_1.contacts.destroy(subject)
+          end
+
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(21)
+            expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
+          end
+        end
+
+        describe 'when final activity itself is destroyed' do
+          before do
+            activity_1.destroy!
+          end
+
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(21)
+            expect(subject.data_integrity_score).to be_within(0.0001).of(0.0943)
+          end
+        end
+
+        describe 'when adding activites after the first one' do
+          it 'does not rescore' do
+            expect(subject).not_to receive(:calculate_score)
+            activity_2.contacts << subject
+          end
+        end
+
+        describe 'when removing activites beyond the first' do
+          let!(:activity_2) { create(:activity_note, contacts: [subject]) }
 
           it 'does not rescore' do
             expect(subject).not_to receive(:calculate_score)
@@ -135,48 +138,55 @@ RSpec.describe Scoreable::Activity, bullet: false do
         # We create instead of build, to ensure the after_save callback fires, giving the correct starting score
         # instead of the random score from the contact factory
         subject { create(:mandate) }
-        let!(:activity_1) { create(:activity_note) }
+        let!(:activity_1) { create(:activity_note, mandates: mandate_list) }
         let!(:activity_2) { create(:activity_note) }
+        let!(:mandate_list) { [] }
 
-        before do
+        before :each do
           activity_1.mandates << subject
         end
 
-        it 'scores correctly when initial activity is added' do
-          expect(subject.data_integrity_missing_fields).not_to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(10)
-          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.4805)
-        end
-
-        it 'scores correctly when final activity is removed' do
-          activity_1.mandates.destroy(subject)
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(11)
-          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2597)
-        end
-
-        it 'scores correctly when final activity itself is destroyed' do
-          activity_1.destroy!
-
-          expect(subject.data_integrity_missing_fields).to include('activities')
-          expect(subject.data_integrity_missing_fields.length).to eq(11)
-          expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2597)
-        end
-
-        it 'does not rescore when adding activites after the first one' do
-          activity_1.save!
-
-          expect(subject).not_to receive(:calculate_score)
-          activity_2.mandates << subject
-        end
-
-        context 'when removing activites (rule: at least one)' do
-          before do
-            activity_1.save!
-            activity_2.mandates << subject
-            activity_2.save!
+        describe 'when initial activity is added' do
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).not_to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(10)
+            expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.4805)
           end
+        end
+
+        describe 'when final activity is removed' do
+          before do
+            activity_1.mandates.destroy(subject)
+          end
+
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(11)
+            expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2597)
+          end
+        end
+
+        describe 'scores correctly when final activity itself is destroyed' do
+          before do
+            activity_1.destroy!
+          end
+
+          it 'scores correctly' do
+            expect(subject.data_integrity_missing_fields).to include('activities')
+            expect(subject.data_integrity_missing_fields.length).to eq(11)
+            expect(subject.data_integrity_partial_score).to be_within(0.0001).of(0.2597)
+          end
+        end
+
+        describe 'when adding activites after the first one' do
+          it 'does not rescore' do
+            expect(subject).not_to receive(:calculate_score)
+            activity_2.mandates << subject
+          end
+        end
+
+        describe 'when removing activites' do
+          let!(:activity_2) { create(:activity_note, mandates: [subject]) }
 
           it 'does not rescore' do
             expect(subject).not_to receive(:calculate_score)
